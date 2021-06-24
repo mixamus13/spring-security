20f0a60ddb2053bd76ade7955e8cb895ecd56c3e
maxim13 <ZLf7XV6s13>
2021-06-24 12:47:13 +0300

    Initial commit

---
+++ src/main/java/com/mixamus/springsecurity/model/Developer.java
@@ -0,0 +1,13 @@
+package com.mixamus.springsecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+
+  private Long id;
+  private String firstName;
+  private String lastName;
+}
---
+++ src/main/java/com/mixamus/springsecurity/model/Role.java
@@ -0,0 +1,6 @@
+package com.mixamus.springsecurity.model;
+
+public enum Role {
+  USER,
+  ADMIN
+}
